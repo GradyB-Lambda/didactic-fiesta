ad11e40424fd2b50aca0306acf2be3574493acdd
unknown <gradybollas@gmail.com>
2021-03-11 09:43:43 -0800

    wip

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('web 39 is better')
+require('dotenv').config()
+
+const express = require('express')
+
+const cors = require('cors')
+
+const server = express()
+
+server.use(express.json())
+
+if(process.env.NODE_ENV === 'production'){
+    const cors = require('cors')
+    server.use(cors())
+}
